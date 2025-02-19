bc0d6ae1c303e2bf0b94a458ad239acd4f687b98
Renan Georgio <43246444+RenanGeorgio@users.noreply.github.com>
2025-02-19 20:39:17 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,13 @@ export default function Home() {
   const InfoCard = (
     <div className="p-4 md:p-8 rounded bg-[#25252d] w-full max-h-[85%] overflow-hidden">
       <h1 className="text-3xl md:text-4xl mb-4">
-        ▲ Next.js + LangChain.js 🦜🔗
+        ▲ Next.js + LangChain.js 🦜🔗 by: RenanGeorgio
       </h1>
       <ul>
         <li className="text-l">
           🤝
           <span className="ml-2">
-            This template showcases a simple chatbot using{" "}
+            Simple chatbot using{" "}
             <a href="https://js.langchain.com/" target="_blank">
               LangChain.js
             </a>{" "}
@@ -22,7 +22,7 @@ export default function Home() {
             <a href="https://nextjs.org/" target="_blank">
               Next.js
             </a>{" "}
-            project.
+            project - by: RenanGeorgio.
           </span>
         </li>
         <li className="hidden text-l md:block">
@@ -37,26 +37,6 @@ export default function Home() {
           <span className="ml-2">
             By default, the bot is pretending to be a pirate, but you can change
             the prompt to whatever you want!
-          </span>
-        </li>
-        <li className="hidden text-l md:block">
-          🎨
-          <span className="ml-2">
-            The main frontend logic is found in <code>app/page.tsx</code>.
-          </span>
-        </li>
-        <li className="text-l">
-          🐙
-          <span className="ml-2">
-            This template is open source - you can see the source code and
-            deploy your own version{" "}
-            <a
-              href="https://github.com/langchain-ai/langchain-nextjs-template"
-              target="_blank"
-            >
-              from the GitHub repo
-            </a>
-            !
           </span>
         </li>
         <li className="text-l">
