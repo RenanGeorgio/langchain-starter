a224197dddb2ac0d9205ba77acae557a07c1b0bd
Renan Georgio <43246444+RenanGeorgio@users.noreply.github.com>
2025-02-19 20:46:55 -0300

    Update page.tsx

--- app/retrieval_agents/page.tsx
+++ app/retrieval_agents/page.tsx
@@ -4,7 +4,7 @@ export default function AgentsPage() {
   const InfoCard = (
     <div className="p-4 md:p-8 rounded bg-[#25252d] w-full max-h-[85%] overflow-hidden">
       <h1 className="text-3xl md:text-4xl mb-4">
-        ▲ Next.js + LangChain.js Retrieval Agent 🦜🔗
+        ▲ Next.js + LangChain.js Retrieval Agent 🦜🔗 - by: RenanGeorgio
       </h1>
       <ul>
         <li className="hidden text-l md:block">
@@ -22,7 +22,7 @@ export default function AgentsPage() {
             <a href="https://nextjs.org/" target="_blank">
               Next.js
             </a>{" "}
-            project.
+            project - by: RenanGeorgio.
           </span>
         </li>
         <li className="hidden text-l md:block">
@@ -45,34 +45,6 @@ export default function AgentsPage() {
           <span className="ml-2">
             By default, the agent is pretending to be a robot, but you can
             change the prompt to whatever you want!
-          </span>
-        </li>
-        <li className="hidden text-l md:block">
-          🎨
-          <span className="ml-2">
-            The main frontend logic is found in{" "}
-            <code>app/retrieval_agents/page.tsx</code>.
-          </span>
-        </li>
-        <li className="text-l">
-          🐙
-          <span className="ml-2">
-            This template is open source - you can see the source code and
-            deploy your own version{" "}
-            <a
-              href="https://github.com/langchain-ai/langchain-nextjs-template"
-              target="_blank"
-            >
-              from the GitHub repo
-            </a>
-            !
-          </span>
-        </li>
-        <li className="hidden text-l md:block">
-          🔱
-          <span className="ml-2">
-            Before running this example, you&apos;ll first need to set up a
-            Supabase (or other) vector store. See the README for more details.
           </span>
         </li>
         <li className="text-l">
